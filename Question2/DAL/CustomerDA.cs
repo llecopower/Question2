aeeae96324800fe24b218fe7af477f804ccb095b
Alex <llecopower@gmail.com>
2022-06-23 15:02:35 -0400

    Homework validations

--- Question2/DAL/CustomerDA.cs
+++ Question2/DAL/CustomerDA.cs
@@ -9,10 +9,24 @@ using System.IO;
 
 namespace Question2.DAL
 {
+
+    //STATIC WILL AVOID TO CREATE INS
    public static  class CustomerDA
     {
-       
-           
+
+        private static string filePath = Application.StartupPath + @"\Customers.dat";
+        private static string fileTemp = Application.StartupPath + @"\Temp.dat";
+
+        //
+        public static void Save(Customer cust)
+        { 
+            StreamWriter sWriter = new StreamWriter(filePath,true);
+            sWriter.WriteLine(cust.CustomerId + "," + cust.FirstName + "," + cust.LastName + ", " + cust.PhoneNumber);
+            sWriter.Close();
+            MessageBox.Show("Custormer Data has been saved!");
+        }
+
+
 
        
     }
